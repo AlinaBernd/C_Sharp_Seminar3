04e3c9636cdc4596ba3e4ee9216ef9455246e34c
alina bernd <a-manaeva@mail.ru>
2023-08-03 21:46:58 +0300

    Добавила решение задачи 21

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-/*
+
 bool Palidrome(string num)
 {
     int i = 0;
@@ -19,16 +19,37 @@ string number = Console.ReadLine();
 bool res = Palidrome(number);
 if (res == true) Console.WriteLine("Yes");
 else Console.WriteLine("No");
-*/
+
 // Задача 21
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double a = x2 - x1;
+    double b = y2 - y1;
+    double c = z2 - z1;
+    return Math.Sqrt(a * a + b * b + c * c);
+}
+Console.Write("Input x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y1: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z1: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input x2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Distance(x2, y2, z2, x1, y1, z1));
 
 // Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
 void WriteCube(int number)
 {
     int current = 1;
@@ -41,3 +62,4 @@ void WriteCube(int number)
 Console.Write("Input a number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 WriteCube (number);
+
